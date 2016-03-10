e1aed1d36d528773f9121351bf0348f555e0ad64
Paul Schoenfelder <paulschoenfelder@gmail.com>
2016-03-10 09:16:35 -0600

    Remove placeholder test

--- test/timex_ecto_test.exs
+++ test/timex_ecto_test.exs
@@ -1,10 +1,6 @@
 defmodule Timex.Ecto.Test do
   use ExUnit.Case
 
-  test "placeholder" do
-    assert true
-  end
-
   test "cast binary time" do
     assert Timex.Ecto.Time.cast("12:30:01") == {:ok, {0, 45001, 0.0}}
   end
