a98eefefb2c56abe80fa4272f566e1221116fb3f
Martin Maillard <self@martin-maillard.com>
2016-03-10 14:37:19 +0100

    Fix undefined Ecto.Time.epoch/0 error

--- test/timex_ecto_test.exs
+++ test/timex_ecto_test.exs
@@ -4,4 +4,8 @@ defmodule Timex.Ecto.Test do
   test "placeholder" do
     assert true
   end
+
+  test "cast binary time" do
+    assert Timex.Ecto.Time.cast("12:30:01") == {:ok, {0, 45001, 0.0}}
+  end
 end
