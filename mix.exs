03666361a32abee1c639fee47f0303801e4a6347
aforward <aforward@gmail.com>
2016-04-25 18:15:03 -0400

    Drop support for elixir 1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TimexEcto.Mixfile do
   def project do
     [app: :timex_ecto,
      version: "1.0.4",
-     elixir: "~> 1.1",
+     elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps,
