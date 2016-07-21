d848a7f06b8610cd57cb65eff1e6b0358e0d7dca
Paul Schoenfelder <paulschoenfelder@gmail.com>
2016-07-21 15:40:55 -0500

    Version 3.0.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule TimexEcto.Mixfile do
 
   def project do
     [app: :timex_ecto,
-     version: "3.0.0",
+     version: "3.0.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
